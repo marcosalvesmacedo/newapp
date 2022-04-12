c1df9764e1b0f6a04b0b496f08e47641e185b23c
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-04-12 15:36:59 -0300

    create an button with counter and segregation components

--- lib/main.dart
+++ lib/main.dart
@@ -7,44 +7,71 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Tutorial',
-      home: TutorialHome(),
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
+      ),
     );
   }
 }
 
-class TutorialHome extends StatelessWidget {
-  const TutorialHome({Key? key}) : super(key: key);
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({required this.count, Key? key}) : super(key: key);
+
+  final int count;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          title: const Text('Example title'),
-          actions: const [
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            )
-          ],
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed, Key? key})
+      : super(key: key);
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Text('Increment'),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() => {
+          ++_counter,
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        CounterIncrementor(onPressed: _increment),
+        const SizedBox(
+          width: 16,
         ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
-        floatingActionButton: const FloatingActionButton(
-          tooltip: 'Add',
-          child: Icon(Icons.add),
-          onPressed: null,
-        ));
+        CounterDisplay(count: _counter),
+      ],
+    );
   }
 }

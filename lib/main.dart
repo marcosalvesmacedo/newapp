df4089adb8297d0b6c12e9d5efb43bd5965ce8b8
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-04-12 10:34:48 -0300

    create an button with container using a GestureDetector

--- lib/main.dart
+++ lib/main.dart
@@ -11,40 +11,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Tutorial',
-      home: TutorialHome(),
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
+      ),
     );
   }
 }
 
-class TutorialHome extends StatelessWidget {
-  const TutorialHome({Key? key}) : super(key: key);
+class MyButton extends StatelessWidget {
+  const MyButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: const IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          title: const Text('Example title'),
-          actions: const [
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            )
-          ],
+    return GestureDetector(
+      onTap: () {
+        print('MyButton as tapped!');
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            color: Colors.lightGreen[500]),
+        child: const Center(
+          child: Text('Engage'),
         ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
-        floatingActionButton: const FloatingActionButton(
-          tooltip: 'Add',
-          child: Icon(Icons.add),
-          onPressed: null,
-        ));
+      ),
+    );
   }
 }

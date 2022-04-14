ec1a65f37f816f6046118ab46c887e7a36d9d96b
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-04-14 11:35:43 -0300

    create an ShoppingList of ShppingListItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
+  debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
@@ -10,14 +12,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ShoppingListItem(
-            product: const Product(name: 'Chips'),
-            inCart: true,
-            onCartChanged: (product, inCart) {},
-          ),
-        ),
+        title: 'Shopping app',
+        home: ShoppingList(
+          products: [
+            Product(name: 'Eggs'),
+            Product(name: 'Flour'),
+            Product(name: 'Chocolate Chips')
+          ],
+        ));
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, Key? key}) : super(key: key);
+
+  final List<Product> products;
+
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shopping list'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 0.8),
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
       ),
     );
   }

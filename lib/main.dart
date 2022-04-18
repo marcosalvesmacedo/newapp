61594798c9706bc606de4b7830c5dac0218ac1ad
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-04-18 12:07:49 -0300

    create a pure hello world

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/rendering.dart';
 
 void main() {
   debugPaintSizeEnabled = true;
-  runApp(const MyApp());
+  runApp(const MyAppNonMaterial());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,100 +12,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Shopping app',
-        home: ShoppingList(
-          products: [
-            Product(name: 'Eggs'),
-            Product(name: 'Flour'),
-            Product(name: 'Chocolate Chips')
-          ],
+        title: 'Flutter layout demonstration',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter layout demonstration'),
+          ),
+          body: const Center(
+            child: Text('Hello World!'),
+          ),
         ));
   }
 }
 
-class ShoppingList extends StatefulWidget {
-  const ShoppingList({required this.products, Key? key}) : super(key: key);
-
-  final List<Product> products;
-
-  @override
-  _ShoppingListState createState() => _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-  final _shoppingCart = <Product>{};
-
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(() {
-      if (!inCart) {
-        _shoppingCart.add(product);
-      } else {
-        _shoppingCart.remove(product);
-      }
-    });
-  }
+class MyAppNonMaterial extends StatelessWidget {
+  const MyAppNonMaterial({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Shopping list'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: 0.8),
-        children: widget.products.map((Product product) {
-          return ShoppingListItem(
-            product: product,
-            inCart: _shoppingCart.contains(product),
-            onCartChanged: _handleCartChanged,
-          );
-        }).toList(),
+    return Container(
+      decoration: const BoxDecoration(color: Colors.white),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
       ),
     );
   }
 }
-
-class ShoppingListItem extends StatelessWidget {
-  ShoppingListItem({
-    required this.product,
-    required this.inCart,
-    required this.onCartChanged,
-  }) : super(key: ObjectKey(product));
-
-  final Product product;
-  final bool inCart;
-  final CartChangedCallback onCartChanged;
-
-  Color _getColor(BuildContext context) {
-    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
-  }
-
-  TextStyle? _getTextStyle(BuildContext context) {
-    if (!inCart) return null;
-
-    return const TextStyle(
-        color: Colors.black54, decoration: TextDecoration.lineThrough);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCartChanged(product, inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
-      ),
-      title: Text(product.name, style: _getTextStyle(context)),
-    );
-  }
-}
-
-class Product {
-  const Product({required this.name});
-
-  final String name;
-}
-
-typedef CartChangedCallback = Function(Product product, bool inCart);

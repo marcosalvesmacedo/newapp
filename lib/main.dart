acb84f52f156f2c47b172c766ba350cadc582a91
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-05-16 21:10:09 -0300

    Create a Listener of mouse scroll

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -17,51 +18,15 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Adaptive and Responsive layout'),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(Insets.small),
-          child: Text(
-            'Hello!',
-            style: TextStyles.h2,
-          ),
+        body: Listener(
+          onPointerSignal: (event) {
+            if (event is PointerScrollEvent) {
+              print(event.scrollDelta.dy);
+            }
+          },
+          child: ListView(),
         ),
       ),
     );
   }
-}
-
-class Insets {
-  static const double xsmall = 3;
-  static const double small = 4;
-  static const double medium = 5;
-  static const double large = 10;
-  static const double extralarge = 20;
-}
-
-class Fonts {
-  static const String raleway = 'Raleway';
-}
-
-class TextStyles {
-  static const TextStyle raleway = TextStyle(
-    fontFamily: Fonts.raleway,
-  );
-  static const TextStyle buttonText1 = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 14,
-  );
-  static const TextStyle buttonText2 = TextStyle(
-    fontWeight: FontWeight.normal,
-    fontSize: 11,
-  );
-  static const TextStyle h1 = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 22,
-  );
-  static const TextStyle h2 = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
-  );
-  static TextStyle body1 = raleway.copyWith(
-    color: const Color(0xFF42A5F5),
-  );
 }

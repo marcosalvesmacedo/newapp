d9921a02e22c2e179eda8d3fff26690d8e7ca210
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-05-26 20:25:13 -0300

    Create a FocusTraversalGroup to organize tabIndex fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -18,55 +17,28 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Adaptive and Responsive layout'),
         ),
-        body: const BasicActionDetector(),
-      ),
-    );
-  }
-}
-
-class BasicActionDetector extends StatefulWidget {
-  const BasicActionDetector({Key? key}) : super(key: key);
-
-  @override
-  _BasicActionDetectorState createState() => _BasicActionDetectorState();
-}
-
-class _BasicActionDetectorState extends State<BasicActionDetector> {
-  bool _hasFocus = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return FocusableActionDetector(
-      onFocusChange: (value) => setState(() => _hasFocus = value),
-      actions: <Type, Action<Intent>>{
-        ActivateIntent: CallbackAction<Intent>(
-          onInvoke: (Intent intent) {
-            print("Enter or Space as pressed!");
-            return null;
-          },
-        ),
-      },
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          const FlutterLogo(size: 100),
-          if (_hasFocus)
-            Positioned(
-              left: 100,
-              top: 100,
-              child: Container(
-                decoration: const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      width: 5.0,
-                      color: Colors.black,
-                    ),
+        body: Column(
+          children: [
+            FocusTraversalGroup(
+              child: Column(
+                children: const [
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Field 1',
+                        hintText: '1'),
                   ),
-                ),
-                child: const Text('Ola Flutter'),
+                ],
               ),
             ),
-        ],
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Field 2',
+                  hintText: '2'),
+            ),
+          ],
+        ),
       ),
     );
   }

7de688ffcadfe531bedd66e0926b9416125f98d2
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2022-06-22 22:08:09 -0300

    Create a componente with differents constrainsts

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,359 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-import 'package:universal_platform/universal_platform.dart';
 
 void main() {
-  debugPaintSizeEnabled = true;
-  runApp(const MyApp());
+  //debugPaintSizeEnabled = true;
+  runApp(const HomePage());
 }
 
-class CreateNewItemIntent extends Intent {
-  const CreateNewItemIntent();
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Adaptive and Responsive layout',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Adaptive and Responsive layout'),
-        ),
-        body: const CreateMouseRegionWithInteraction(),
-      ),
+    return const FlutterLayoutArticle(
+      [
+        Example1(),
+        Example2(),
+        Example3(),
+        Example4(),
+        Example5(),
+        Example6(),
+      ],
     );
   }
 }
 
-class CreateMouseRegionWithInteraction extends StatefulWidget {
-  const CreateMouseRegionWithInteraction({Key? key}) : super(key: key);
+abstract class Example extends StatelessWidget {
+  const Example({Key? key}) : super(key: key);
 
-  @override
-  _MouseRegionState createState() => _MouseRegionState();
+  String get code;
+
+  String get explanation;
 }
 
-class _MouseRegionState extends State<CreateMouseRegionWithInteraction> {
-  bool _isMouseOver = false;
+class FlutterLayoutArticle extends StatefulWidget {
+  const FlutterLayoutArticle(this.examples, {Key? key}) : super(key: key);
+
+  final List<Example> examples;
+
+  @override
+  _FlutterLayoutArticleState createState() => _FlutterLayoutArticleState();
+}
+
+class _FlutterLayoutArticleState extends State<FlutterLayoutArticle> {
+  late int count;
+  late Widget example;
+  late String code;
+  late String explanation;
+
+  @override
+  void initState() {
+    count = 1;
+    code = const Example1().code;
+    explanation = const Example1().explanation;
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(FlutterLayoutArticle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    var example = widget.examples[count - 1];
+    code = example.code;
+    explanation = example.explanation;
+  }
 
   @override
   Widget build(BuildContext context) {
-    TextDirection btnDirection =
-        UniversalPlatform.isWindows ? TextDirection.rtl : TextDirection.ltr;
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      onEnter: (_) => onEntered(true),
-      onExit: (_) => onEntered(false),
-      onHover: (e) => print(e.localPosition),
-      child: GestureDetector(
-        onTap: () {
-          Focus.of(context).requestFocus();
-        },
-        child: Container(
-          height: 500,
-          color: _isMouseOver ? Colors.blue : Colors.black,
-          child: Column(
-            children: [
-              const Tooltip(
-                message: 'I am a Tooltip',
-                child: Text('Hover over the text to show a tooltip.'),
-              ),
-              const SelectableText('i am a selectable text'),
-              const SelectableText.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Hello',
-                    ),
-                    TextSpan(
-                      text: 'Bold',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              Row(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Layout Article',
+      home: SafeArea(
+        child: Material(
+          color: Colors.black,
+          child: FittedBox(
+            child: Container(
+              width: 400,
+              height: 670,
+              color: const Color(0xFFCCCCCC),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Spacer(),
-                  Row(
-                    textDirection: btnDirection,
-                    children: [
-                      TextButton(
-                        onPressed: () => {},
-                        child: const Text('OK'),
-                      ),
-                      TextButton(
-                        onPressed: () => {},
-                        child: const Text('Cancel'),
-                      )
-                    ],
+                  Expanded(
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints.tightFor(
+                          width: double.infinity, height: double.infinity),
+                      child: widget.examples[count - 1],
+                    ),
                   ),
+                  Container(
+                    height: 50,
+                    width: double.infinity,
+                    color: Colors.black,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          for (int i = 0; i < widget.examples.length; i++)
+                            Container(
+                              width: 58,
+                              padding:
+                                  const EdgeInsets.only(left: 4.0, right: 4.0),
+                              child: button(i + 1),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Scrollbar(
+                        child: SingleChildScrollView(
+                      key: ValueKey(count),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Center(
+                              child: Text(code),
+                            ),
+                            const SizedBox(height: 15),
+                            Text(
+                              explanation,
+                              style: TextStyle(
+                                  color: Colors.blue[900],
+                                  fontStyle: FontStyle.italic),
+                            )
+                          ],
+                        ),
+                      ),
+                    )),
+                    height: 273,
+                    color: Colors.grey[50],
+                  )
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  void onEntered(bool isHovered) {
-    setState(() => _isMouseOver = isHovered);
+  Widget button(int exampleNumber) {
+    return Button(
+      key: ValueKey('button$exampleNumber'),
+      isSelected: count == exampleNumber,
+      exampleNumber: exampleNumber,
+      onPressed: () {
+        showExample(
+          exampleNumber,
+          widget.examples[exampleNumber - 1].code,
+          widget.examples[exampleNumber - 1].explanation,
+        );
+      },
+    );
+  }
+
+  void showExample(int exampleNumber, String code, String explanation) {
+    setState(() {
+      count = exampleNumber;
+      this.code = code;
+      this.explanation = explanation;
+    });
+  }
+}
+
+//////////////////////////////////////////////////
+
+class Button extends StatelessWidget {
+  final bool isSelected;
+  final int exampleNumber;
+  final VoidCallback onPressed;
+
+  const Button({
+    required Key key,
+    required this.isSelected,
+    required this.exampleNumber,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        primary: Colors.white,
+        backgroundColor: isSelected ? Colors.grey : Colors.grey[800],
+      ),
+      child: Text(exampleNumber.toString()),
+      onPressed: () {
+        Scrollable.ensureVisible(
+          context,
+          duration: const Duration(milliseconds: 350),
+          curve: Curves.easeOut,
+          alignment: 0.5,
+        );
+        onPressed();
+      },
+    );
+  }
+}
+
+//////////////////////////////////////////////////
+
+class Example1 extends Example {
+  const Example1({Key? key}) : super(key: key);
+
+  @override
+  final code = 'Container(color: red)';
+
+  @override
+  final explanation = 'The screen is the parent of the Container, '
+      'and it forces the Container to be exactly the same size as the screen.'
+      '\n\n'
+      'So the Container fills the screen and paints it red.';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
+
+//////////////////////////////////////////////////
+
+class Example2 extends Example {
+  const Example2({Key? key}) : super(key: key);
+
+  @override
+  final code = 'Container(width: 100, height: 100, color: red)';
+
+  @override
+  final explanation = 'The red Container wants to be 100x100, but it can\'t, '
+      'because the screen forces it to be exactly the same size as the screen.'
+      '\n\n'
+      'So the Container fills the screen.';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 100, height: 100, color: Colors.red);
+  }
+}
+//////////////////////////////////////////////////
+
+class Example3 extends Example {
+  const Example3({Key? key}) : super(key: key);
+
+  @override
+  final code = 'Center(\n'
+      '   child: Container(width: 100, height: 100, color: red))';
+
+  @override
+  final explanation =
+      'The screen forces the Center to be exactly the same size as the screen, '
+      'so the Center fills the screen.'
+      '\n\n'
+      'The Center tells the Container that it can be any size it wants, but not bigger than the screen.'
+      'Now the Container can indeed be 100x100.';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+//////////////////////////////////////////////////
+
+class Example4 extends Example {
+  const Example4({Key? key}) : super(key: key);
+
+  @override
+  final code = 'Align(\n'
+      '   alignment: Alignment.bottomRight,\n'
+      '   child: Container(width: 100, height: 100, color: red))';
+
+  @override
+  final explanation =
+      'This is different from the previous example in that it uses Align instead of Center.'
+      '\n\n'
+      'Align also tells the Container that it can be any size it wants, but if there is empty space it won\'t center the Container. '
+      'Instead, it aligns the Container to the bottom-right of the availab';
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class Example5 extends Example {
+  const Example5({Key? key}) : super(key: key);
+
+  @override
+  final code = 'Center(\n'
+      '   child: Container(\n'
+      '              color: red,\n'
+      '              width: double.infinity,\n'
+      '              height: double.infinity))';
+
+  @override
+  final explanation =
+      'The screen forces the Center to be exactly the same size as the screen, '
+      'so the Center fills the screen.'
+      '\n\n'
+      'The Center tells the Container that it can be any size it wants, but not bigger than the screen.'
+      'The Container wants to be of infinite size, but since it can\'t be bigger than the screen, it just fills the screen.';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class Example6 extends Example {
+  const Example6({Key? key}) : super(key: key);
+
+  @override
+  final code = 'Center(child: Container(color: red))';
+
+  @override
+  final explanation =
+      'The screen forces the Center to be exactly the same size as the screen, '
+      'so the Center fills the screen.'
+      '\n\n'
+      'The Center tells the Container that it can be any size it wants, but not bigger than the screen.'
+      '\n\n'
+      'Since the Container has no child and no fixed size, it decides it wants to be as big as possible, so it fills the whole screen.'
+      '\n\n'
+      'But why does the Container decide that? '
+      'Simply because that\'s a design decision by those who created the Container widget. '
+      'It could have been created differently, and you have to read the Container documentation to understand how it behaves, depending on the circumstances. ';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
   }
 }
